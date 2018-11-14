f8d03cba6ffbcd746ed911aebd70262b0ad11d67
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-11-14 01:05:17 -0200

    containers: add sidemenu

--- src/Pages/Logged/index.js
+++ src/Pages/Logged/index.js
@@ -4,39 +4,21 @@ import { Route, Switch } from 'react-router-dom'
 import Home from '../Home'
 import Products from  './Products'
 import Orders from './Orders'
+import SideMenu from '../../Containers/SideMenu'
 
-const { Header, Sider, Content } = Layout
+const { Header, Content } = Layout
+
 
 class Logged extends Component {
   state = {
     collapsed: false,
-  };
+  }
 
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     });
   }
-
-  renderSide = () => (
-    <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-    <div className="logo" />
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-      <Menu.Item key="1">
-        <Icon type="user" />
-        <span>nav 1</span>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Icon type="video-camera" />
-        <span>nav 2</span>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Icon type="upload" />
-        <span>nav 3</span>
-      </Menu.Item>
-    </Menu>
-  </Sider>
-  )
 
   renderHeader = () => (
     <Header style={{ background: '#fff', padding: 0 }}>
@@ -50,7 +32,7 @@ class Logged extends Component {
   render() {
     return (
       <Layout style={{overflow: 'inital', minHeight: '100vh'}}>
-        {this.renderSide()}
+        <SideMenu collapsed={this.state.collapsed}/>
         <Layout>
           {this.renderHeader()}
           <Content 
