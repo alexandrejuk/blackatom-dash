f8d8eaafc2fe57d8ceaae09f6b81631a76775826
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-11-20 13:31:16 -0200

    ui/ux: add message error and success

--- src/Pages/Logged/Products/New/index.js
+++ src/Pages/Logged/Products/New/index.js
@@ -5,35 +5,34 @@ import { message } from 'antd'
 import ProductForm from '../../../../Containers/Product/Form'
 import productService from '../../../../services/products'
 
-
 const success = () => {
-  message.success('Produto cadastrado com sucesso!');
-};
+  message.success('Produto cadastrado com sucesso!')
+}
 
 const error = () => {
-  message.error('Não foi possível cadastrar o produto!');
-};
+  message.error('Não foi possível cadastrar o produto!')
+}
 
 class NewProduct extends Component {
-  async handleOnSubmit(values) {
+  state = {
+    formKey: 0
+  }
+
+  handleOnSubmit = async (values) => {
     try {
       await productService.addProduct(values)
       success()
-    }catch {
+      this.setState((state) => ({ formKey: state.formKey + 1 }))
+    }catch(err) {
       error()
     }
   }
-
-  messageHandle = () => {
-
-  }
-
 
   render() { 
     return (
       <div className="wrapperNewProduct">
         <h1 className="newProductTitle">Cadastrar Novo Produto</h1>
-        <ProductForm actionLabel="Salvar" onSubmit={this.handleOnSubmit}/>    
+        <ProductForm key={this.state.formKey} actionLabel="Salvar" onSubmit={this.handleOnSubmit}/>    
       </div>
     )
   }
