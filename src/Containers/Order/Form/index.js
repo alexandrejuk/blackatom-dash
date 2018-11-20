90b21a6660a32114f6bafdd1e1037c3e53ac3f5f
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-11-20 14:01:58 -0200

    ui/ux: add message success and error , reset form order

--- src/Containers/Order/Form/index.js
+++ src/Containers/Order/Form/index.js
@@ -8,6 +8,8 @@ import OrderProductTable from './OrderProductTable'
 
 class NewOrder extends Component {
   orderFormRef = null
+  productFormRef = null
+
   state = {
     productsTable: []
   }
@@ -16,6 +18,7 @@ class NewOrder extends Component {
     this.setState({
       productsTable: [...this.state.productsTable, value]
     })
+    this.productFormRef.props.form.resetFields()
   }
 
   handleRemoveProduct = (index) => {
@@ -38,12 +41,12 @@ class NewOrder extends Component {
   render() {
     const { products, stockLocations } = this.props
     return (
-      <>
+      <div>
         <OrderForm wrappedComponentRef={(form) => this.orderFormRef = form} stockLocations={stockLocations} />
-        <OrderProductForm  products={products} onSubmit={this.handleProductsTable} />
+        <OrderProductForm wrappedComponentRef={(form) => this.productFormRef = form} products={products} onSubmit={this.handleProductsTable} />
         <OrderProductTable handleRemoveProduct={this.handleRemoveProduct} products={this.state.productsTable}/>
-        <Button onClick={this.onSave}> Salvar </Button>
-      </>
+        <Button type="primary" htmlType="submit" onClick={this.onSave}> Salvar </Button>
+      </div>
     )
   }
 }
