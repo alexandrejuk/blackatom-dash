3f4583294ec1f813e5438fec56a5caf20a39478a
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-11-26 12:09:36 -0200

    chore: add import editProductAvailable

--- src/Pages/Logged/Stock/Available/index.js
+++ src/Pages/Logged/Stock/Available/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import NewProductAvailable from './New'
+import EditProductAvailable from './Edit'
 import IndividualProductList from './List'
 
 const Available = () => (
   <Switch>
     <Route path="/logged/stock/available/new" component={NewProductAvailable}/>
-    <Route path="/logged/stock/available/edit/:id" component={() => <h1>edit available</h1>}/>
+    <Route path="/logged/stock/available/edit/:id" component={EditProductAvailable}/>
     <Route path="/logged/stock/available/list" component={IndividualProductList}/>
   </Switch>
 )
