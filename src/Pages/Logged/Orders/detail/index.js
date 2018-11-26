d868266691df67ae9c1a0f90eba35d5b53d0eabe
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-11-26 10:25:24 -0200

    individual-product: created modal to add serial number

--- src/Pages/Logged/Orders/detail/index.js
+++ src/Pages/Logged/Orders/detail/index.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
 import orderService from '../../../../services/orders'
+import individualProductService from '../../../../services/individualProduct'
+
 import './index.css'
 import iconCheck from '../../../../assets/icon/checked.svg'
 import iconCancell from '../../../../assets/icon/delete.svg'
+import { uniqWith, eqBy } from 'ramda'
 
-import { Col, Row, Button, Alert } from 'antd'
+import { Col, Row, Button, Alert, Modal, Input } from 'antd'
 import ProductList from '../../../../Containers/Order/ProductList'
+
+const TextArea = Input.TextArea
 
 class OrderList extends Component {
   state = { 
-    order: {}
+    order: {},
+    loading: false,
+    visible: false,
+    serialNumbersText: '',
+    productModal: null,
   }
 
   componentDidMount() {
     this.getOrder()
+  }
+
+  showModal = (productModal) => {
+    this.setState({ visible: true, productModal })
+  }
+
+  handleCancel = () => {
+    this.setState({ visible: false })
+  }
+
+  handleOk = () => {
+    this.setState({ visible: false })
+    this.handleClick()
   }
 
   async getOrder() {
@@ -25,6 +47,54 @@ class OrderList extends Component {
     this.setState({ order })
   }
 
+
+  getSerialNumbers = () => {
+    const serialArray = this.state.serialNumbersText
+      .split("\n")
+      .filter(s => s !== "")
+
+    return uniqWith(eqBy(String))(serialArray)
+  }
+
+  handleClick = async () => {
+    const individualProductData = {
+      productId: this.state.productModal.productId,
+      originId: this.state.productModal.id,
+      originType: 'orderProduct',
+      stockLocationId: this.state.order.stockLocationId,
+      serialNumbers: this.getSerialNumbers()
+    }
+    try {
+      await individualProductService
+        .addManyProductsSerialNumber(individualProductData)
+        this.setState({ productModal: null, serialNumbersText: '' })
+    } catch (error) {
+      console.log(error)
+    }    
+  }
+
+  handleOnChange = (e) => {
+    const serialNumbersText = e.target.value
+    this.setState({ serialNumbersText })
+  }
+
+  renderModal = () => {
+    const product = this.state.productModal ? this.state.productModal.product.name : 'Product not Selected!'
+    return (
+      <Modal
+        visible={this.state.visible}
+        title={product}
+        onCancel={this.handleCancel}
+        footer={[
+          <Button key="back" onClick={this.handleCancel}>Voltar</Button>,
+          <Button key="submit" onClick={this.handleOk} type="primary" >Salvar</Button>,
+        ]}
+      >
+        <TextArea rows={4} value={this.state.serialNumbersText} onChange={this.handleOnChange} />
+      </Modal>
+    )
+  }
+  
   handleCancellOrder = async () => {
     const id = this.props.match.params.id
     const order = await orderService
@@ -71,7 +141,7 @@ class OrderList extends Component {
           <Row className="orderProducts">
             <Col>
               <span className="productsOrderTitle">PRODUTOS DA COMPRA</span>
-              <ProductList orderProducts={order.orderProducts} />
+              <ProductList orderProducts={order.orderProducts} onClick={this.showModal}/>
             </Col>
           </Row>
         </div>
@@ -82,7 +152,7 @@ class OrderList extends Component {
             <Alert type="error" message="Compra Cancelada" banner />
           }
         </div>
-        {JSON.stringify( )}
+        {this.renderModal()}
       </div>
     )
   }
