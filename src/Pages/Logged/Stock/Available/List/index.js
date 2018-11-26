faef940a6dfdc57a805ee69849d0b20774081ce2
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-11-26 10:25:58 -0200

    individual-product: created list product and add product

---
+++ src/Pages/Logged/Stock/Available/List/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'antd'
+
+import IndividualProductListContainer from '../../../../../Containers/IndividualProduct/List'
+import individualProductService from '../../../../../services/individualProduct'
+
+import './index.css'
+
+class IndividualProductList extends Component {
+  state = { 
+    individualProducts: []
+  }
+
+  componentDidMount() {
+    this.getIndividualProducts()
+  }
+
+  getIndividualProducts = async () => {
+    const individualProducts = await individualProductService.individualProductList()
+        .then(response => response.data)
+    this.setState({ individualProducts })
+  }
+
+  render() { 
+    return (
+      <div className="wrapperProducts">
+        <div className="productListHeader">
+          <h1>Gerenciar Produtos Disponíveis</h1>
+            <Link className="linkNav" to={'/logged/stock/available/new'}>
+              <Button  type="primary"> Novo </ Button>
+            </Link>
+        </div>
+        <div className="productListContent"><IndividualProductListContainer individualProducts={this.state.individualProducts} /></div>
+      </div>
+    )
+  }
+}
+ 
+export default IndividualProductList
