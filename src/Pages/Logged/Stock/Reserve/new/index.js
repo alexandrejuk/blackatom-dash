a515482aba5eefa5627b2320a164613de622c8b4
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-12-13 17:49:10 -0200

    pages: refactor on components

--- src/Pages/Logged/Stock/Reserve/new/index.js
+++ src/Pages/Logged/Stock/Reserve/new/index.js
@@ -1,89 +1,42 @@
 import React, { Component } from 'react'
 import './index.css'
-import { Radio, Button, Form, Input } from 'antd'
 
-const FormItem = Form.Item
+import NewReserveContainer from '../../../../../Containers/Reserve/Form'
+
+import productService from '../../../../../services/products'
+import stockLocationService from '../../../../../services/stockLocation'
 
 class NewReserve extends Component {
+  state = {
+    stockLocations: [],
+    products: []
+  }
+  componentDidMount() {
+    this.getProducts()
+    this.getStockLocations()
+  }
+
+  async getProducts() {
+    const products = await productService
+      .productList()
+      .then(response => response.data)
+    this.setState({ products })
+  }
+  async getStockLocations() {
+    const stockLocations = await stockLocationService
+      .getStockLocations()
+      .then(response => response.data)
+    this.setState({ stockLocations })
+  }
 
   render() { 
     return (
-      <div className="wrapperNewReserve">
-        <h1>Criar Nova Reserva</h1>
-        <Form layout="inline">
-  
-          <div className="sectionLabel">
-            <h3>Dados Cliente</h3>
-          </div>
-          <FormItem
-            label="Cnpj">
-            <Input style={{ width: 200 }}/>
-          </FormItem>
-          <FormItem
-            label="Razão Social">
-            <Input style={{ width: 500 }}/>
-          </FormItem>
-          <div className="sectionLabel">
-            <h3>Tipo de Reserva</h3>
-          </div>
-          <FormItem
-            label="Selecione o Tipo">
-            <Radio.Group defaultValue="" buttonStyle="solid">
-              <Radio.Button value="d">Atendimento</Radio.Button>
-              <Radio.Button value="a">Correios</Radio.Button>
-              <Radio.Button value="c">Cliente Retira</Radio.Button>
-              <Radio.Button value="b">Mercado Livre</Radio.Button>
-              <Radio.Button value="e">Laboratório</Radio.Button>
-            </Radio.Group>
-          </FormItem>
-          <div className="sectionLabel">
-            <h3>Produto Reserva</h3>
-          </div>   
-          <FormItem
-            label="Produto">
-            <Input style={{ width: 500 }}/>
-          </FormItem>
-          <FormItem
-            label="Quantidade">
-            <Input style={{ width: 100 }}/>
-          </FormItem>
-          <FormItem>
-            <Button>Adicionar</Button>
-          </FormItem>
-        </Form>
-
-        <div className="sectionLabel product-reserve">
-          <h3>Produtos Selecionados</h3>
-        </div>
-
-        <div className="card-product-reserve">
-          <div className="card-product-reserve-title">
-            <h3 className="no-margin title-card">PRISMA SF RO2</h3>
-            <p className="no-margin subtitle-card">HENRY</p>
-          </div>
-          <div className="card-product-reserve-title">
-            <h3 className="no-margin title-card">5 UN</h3>
-          </div>
-          <div className="card-product-reserve-title">
-            <Button className="buttonRemoveItem" type="danger">Remover</Button>
-          </div>
-        </div>
-
-        <div className="card-product-reserve">
-          <div className="card-product-reserve-title">
-            <h3 className="no-margin title-card">PRISMA SF RO2</h3>
-            <p className="no-margin subtitle-card">HENRY</p>
-          </div>
-          <div className="card-product-reserve-title">
-            <h3 className="no-margin title-card">5 UN</h3>
-          </div>
-          <div className="card-product-reserve-title">
-            <Button className="buttonRemoveItem" type="danger">Remover</Button>
-          </div>
-        </div>
-      </div>
+      <NewReserveContainer 
+        products={this.state.products} 
+        stockLocations={this.state.stockLocations}
+      />
     )
   }
 }
  
-export default Form.create()(NewReserve)
+export default NewReserve
