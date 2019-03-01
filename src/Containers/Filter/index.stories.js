8cb90afc9ab1e9cb80ad8e8a1314a8c92c638f10
vitorsilvalima <vitorsilvalima29@gmail.com>
2019-02-28 23:57:23 -0300

    containers: chance filter implementation

--- src/Containers/Filter/index.stories.js
+++ src/Containers/Filter/index.stories.js
@@ -4,11 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Filter from '.'
 
-const filters = [
-  { property: 'name', type: 'text', label: 'Name', width: "500px" },
-  { property: 'age', type: 'number', label: 'Age', width: "200px" },
-  { property: 'gender', type: 'text', label: 'Gender', width: "200px" },
-]
 
 storiesOf('Filter', module)
-  .add('filter default', () => <Filter filters={filters} />)
+  .add('filter default', () => <Filter
+    onSearch={action('onSearch')}
+    placeholder='nome do produto'
+  />)
